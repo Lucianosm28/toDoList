712f6c829d8bfb2dbae92d0e25497f10ad7b6997
unknown <lsome@live.com>
2024-03-13 19:29:04 -0300

    Criando uma tabela

--- src/main/java/br/com/lucianomelo/todolist/user/UserModel.java
+++ src/main/java/br/com/lucianomelo/todolist/user/UserModel.java
@@ -1,13 +1,30 @@
 package br.com.lucianomelo.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data     //Automaticamente coloca os Getters e Setters
           //@Getter se for querer apenas os Getters
           //@Setter se for querer apenas os Setters
 
+@Entity(name = "tb_users")          
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID" )           //O JPA que vai gerenciar o id
+    private UUID id;
+
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
