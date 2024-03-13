1ad87067172cf41999c167ffd0c2e0ace74b591f
unknown <lsome@live.com>
2024-03-13 20:05:30 -0300

    Validando username

--- src/main/java/br/com/lucianomelo/todolist/user/UserModel.java
+++ src/main/java/br/com/lucianomelo/todolist/user/UserModel.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -21,6 +22,7 @@ public class UserModel {
     @GeneratedValue(generator = "UUID" )           //O JPA que vai gerenciar o id
     private UUID id;
 
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
