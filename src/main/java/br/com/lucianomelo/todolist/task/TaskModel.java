a73a2897192d97a350d982c87f948846a5d52786
unknown <lsome@live.com>
2024-03-14 01:13:37 -0300

    Tabela de Task

---
+++ src/main/java/br/com/lucianomelo/todolist/task/TaskModel.java
@@ -0,0 +1,46 @@
+package br.com.lucianomelo.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/**
+ * 
+ * ID
+ * Usuário (ID_USUARIO)
+ * Descrição
+ * Título
+ * Data de Início
+ * Data de término
+ * Prioridade
+ */
+
+@Data                       //Getters e Setters
+@Entity(name = "tb_tasks")  //Entidade
+public class TaskModel {
+ 
+ @Id                        //Chave primária
+ @GeneratedValue(generator = "UUID")  //Gerar automaticamente id
+ private UUID id;
+ private String description;
+
+ @Column(length = 50)       //Limitar o campo "title" em 50 caracteres 
+ private String title;
+ private LocalDateTime startAt;
+ private LocalDateTime endAt;
+ private String priority;
+
+ private UUID idUser;
+
+
+ @CreationTimestamp       //Quando a task foi criada no banco de dados
+ private LocalDateTime createdAt;
+
+}
