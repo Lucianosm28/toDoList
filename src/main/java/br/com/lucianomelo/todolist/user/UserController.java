d90a34609fc7b14d2ee3c40eee06408558c9d523
unknown <lsome@live.com>
2024-03-13 23:01:18 -0300

    Utilizando ResponseEntity

--- src/main/java/br/com/lucianomelo/todolist/user/UserController.java
+++ src/main/java/br/com/lucianomelo/todolist/user/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -23,16 +25,15 @@ public class UserController {
     private IUserRepository userRepository; //Aqui chama a interface criada
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if(user != null) {
-            System.out.println("Usuário já existe");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
 
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;    //retorna o usuário que foi criado
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);    //retorna o usuário que foi criado
     }
     
 }
