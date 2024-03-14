6b28945fd53c03644ad641dd4db014e3f732dc5b
unknown <lsome@live.com>
2024-03-14 01:42:47 -0300

    Cadastro de tarefa

---
+++ src/main/java/br/com/lucianomelo/todolist/task/TaskController.java
@@ -0,0 +1,26 @@
+package br.com.lucianomelo.todolist.task;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController                //Controller
+@RequestMapping("/tasks")      //Caminho da task
+
+public class TaskController {
+
+    @Autowired                  //Spring gerencia a instância do Repository
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public TaskModel create(@RequestBody TaskModel taskModel) {  //RequestBody para saber que virá do corpo da aplicação os dados
+        var task = this.taskRepository.save(taskModel);
+        return task;
+    }
+
+
+    
+}
