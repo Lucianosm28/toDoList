aea01a4606c6bb205d235e39f90d7c7e8a6249c0
unknown <lsome@live.com>
2024-03-15 00:07:56 -0300

    Continuando a Implementação do Filtro

--- src/main/java/br/com/lucianomelo/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/lucianomelo/todolist/filter/FilterTaskAuth.java
@@ -1,26 +1,60 @@
 package br.com.lucianomelo.todolist.filter;
 
 import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component  // Para que o Spring gerencie essa classe
-public class FilterTaskAuth implements Filter {
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
 
-        //Executar alguma ação
-        System.out.println("Chegou no filtro");
-        chain.doFilter(request, response); //Na requisição aqui determina se pode seguir adiante
+    @Override 
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+    
+
+        //Pegar autenticação(usuário e senha)
+        var authorization = request.getHeader("Authorization");
+
+        var authEncoded = authorization.substring("Basic".length()).trim(); //Vai tirar o "Basic" do print e remover o espaço em branco
+
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded);        // Vai decodificar, converter o código para ano print aparecer o usuário e senha no console
+        
+        var authString = new String(authDecode);
+        
+
+        //["cristianocr7", "7777"]
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+
+        System.out.println("Authorization");
+        System.out.println(username);
+        System.out.println(password);
+
+
+        //Validar usuário
+
+        //Validar senha
+
+        //Segue viagem
+
+        filterChain.doFilter(request, response);
+    
+    }
+   
+
     }
     
-}
+
---
+++ src/main/java/br/com/lucianomelo/todolist/filter/OncePerRequestFilter.java
@@ -0,0 +1,5 @@
+package br.com.lucianomelo.todolist.filter;
+
+public interface OncePerRequestFilter {
+
+}
